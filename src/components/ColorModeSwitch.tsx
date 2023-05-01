53a967c57f88e4cfa769630db6e1aec59d99dc3d
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-05-01 23:17:13 +0100

    enhancing the switch color mode

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,11 +1,12 @@
 import {HStack, Switch, Text, useColorMode} from "@chakra-ui/react";
+import React, {useState} from "react";
 
-export const ColorModeSwitch = () => {
-    const {colorMode, toggleColorMode} =  useColorMode();
+export const ColorModeSwitch: React.FC<{handleToggleColor: () => void, colorMode: string}> = ({handleToggleColor, colorMode}) => {
+
     return (
         <HStack>
-            <Switch colorScheme='green' isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
-            <Text>Dark Mode</Text>
+            <Switch colorScheme='green' isChecked={colorMode === 'dark'} onChange={handleToggleColor} />
+            <Text fontFamily='monospace' fontWeight='extrabold' fontSize="14px">Dark Mode</Text>
         </HStack>
     );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,22 @@
-import {HStack, Image, Text} from "@chakra-ui/react";
+import {HStack, Image, Text, useColorMode} from "@chakra-ui/react";
 import logo from '../assets/logo.webp';
 import {ColorModeSwitch} from "./ColorModeSwitch";
+import {useState} from "react";
+
 
 export const NavBar = () => {
+    const {colorMode, toggleColorMode} =  useColorMode();
+
     return (
-        <HStack justifyContent="space-between" paddingRight="10px" bg='gray.700' borderBottom='1px' borderColor='whitesmoke'>
+        <HStack justifyContent="space-between" paddingRight="10px" borderBottom='1px'
+                borderColor={colorMode === 'dark' ? 'whitesmoke' : 'gray.700'}
+                bg={colorMode === 'dark' ? 'gray.700' : 'gray.100'}
+        >
             <HStack>
                 <Image src={logo} boxSize='60px' />
-                <Text fontFamily='monospace' fontSize='14px' fontWeight='extrabold'>NizarIng</Text>
+                <Text fontFamily='monospace' fontSize='18px' fontWeight='extrabold' color={colorMode === 'dark' ? 'whitesmoke' : 'gray.800'}>INizar&copy;2023</Text>
             </HStack>
-            <ColorModeSwitch />
+            <ColorModeSwitch handleToggleColor={toggleColorMode} colorMode={colorMode} />
         </HStack>
     );
 };

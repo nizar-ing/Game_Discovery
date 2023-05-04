8ececa081c8f808130d211d0f8f6f0bd51db4fdf
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-05-04 19:01:45 +0100

    additional enhancing

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -12,7 +12,7 @@ export const GenreList: React.FC<{selectedGenre: Genre | null, colorMode: ColorM
         <List>
             { data.map((genre) => (
                 <ListItem key={genre.id} paddingY='5px' bg={(colorMode === 'dark') && (genre.id === selectedGenre?.id) ?
-                                                            'gray.700' : (colorMode === 'light') && (genre.id === selectedGenre?.id) ? 'whitesmoke' : ''}>
+                                                            'gray.700' : (colorMode === 'light') && (genre.id === selectedGenre?.id) ? 'gray.100' : ''}>
                     <HStack>
                         <Image boxSize='32px' borderRadius='8' src={getCroppedImageUrl(genre.image_background)} />
                         <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} fontSize='lg' variant='link' onClick={() => onSelectedGenre(genre)}>{genre.name}</Button>

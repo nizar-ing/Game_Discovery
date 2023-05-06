9975394fef7a16ad441d42b69e61892fa39201bf
Nizar ILAHI <nizar.ilahi@gmail.com>
2023-05-06 19:14:55 +0100

    Refactor Game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,3 @@
-
 import {SimpleGrid, Text} from "@chakra-ui/react";
 import {useGames} from "../hooks/useGames";
 import {GameCard} from "./GameCard";
@@ -12,26 +11,24 @@ import {PlatformModel} from "../models/Platform.Model";
 import GameQuery from "../queryObjects/GameQuery";
 
 
-export const GameGrid: React.FC<{gameQuery: GameQuery}> = ({gameQuery}) => {
+export const GameGrid: React.FC<{ gameQuery: GameQuery }> = ({gameQuery}) => {
     const {data, error, isLoading} = useGames(gameQuery);
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+    if (error) return <Text>{error}</Text>;
     return (
-        <>
-            { error && <Text>{error}</Text>}
-            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} spacing={6} padding="10px">
-                {isLoading && skeletons.map((skeleton) => (
-                                                    <GameCardContainer key={skeleton}>
-                                                        <GameCardSkeleton />
-                                                    </GameCardContainer>
-                                                )
-                    )}
-                {data.map((game) => (
-                                                <GameCardContainer key={game.id}>
-                                                    <GameCard game={game} />
-                                                </GameCardContainer>
-                                            )
-                )}
-            </SimpleGrid>
-        </>
+        <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} spacing={6} padding="10px">
+            {isLoading && skeletons.map((skeleton) => (
+                    <GameCardContainer key={skeleton}>
+                        <GameCardSkeleton/>
+                    </GameCardContainer>
+                )
+            )}
+            {data.map((game) => (
+                    <GameCardContainer key={game.id}>
+                        <GameCard game={game}/>
+                    </GameCardContainer>
+                )
+            )}
+        </SimpleGrid>
     );
 };
